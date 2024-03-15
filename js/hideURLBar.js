9f3831867ca405a729779049ee8ada39d7a8002c
BarbaraLeimig <barbaraleimigdev@gmail.com>
2024-03-15 15:27:02 -0300

    correção event listener

--- js/hideURLBar.js
+++ js/hideURLBar.js
@@ -1,13 +1,9 @@
-addEventListener(
-    "load",
-    function () {
-      // Chama a função hideURLbar assim que a página é carregada.
-      setTimeout(hideURLbar, 0);
-    },
-    false
-  );
+document.addEventListener("DOMContentLoaded", function init() {
+  // Oculta a barra de URL do navegador assim que a página é carregada.
+  setTimeout(hideURLbar, 0);
+});
 
-  function hideURLbar() {
-    // Move a janela de visualização para esconder a barra de URL do navegador.
-    window.scrollTo(0, 1);
-  }
+function hideURLbar() {
+  // Move a janela de visualização para esconder a barra de URL do navegador.
+  window.scrollTo(0, 1);
+}
