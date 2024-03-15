eee509a21c158376c2299519a3e8043928da64c2
BarbaraLeimig <barbaraleimigdev@gmail.com>
2024-03-15 15:30:18 -0300

    atualização

--- js/hideURLBar.js
+++ js/hideURLBar.js
@@ -1,9 +1,13 @@
-document.addEventListener("DOMContentLoaded", function init() {
-  // Oculta a barra de URL do navegador assim que a página é carregada.
-  setTimeout(hideURLbar, 0);
-});
+addEventListener(
+    "load",
+    function () {
+      // Chama a função hideURLbar assim que a página é carregada.
+      setTimeout(hideURLbar, 0);
+    },
+    false
+  );
 
-function hideURLbar() {
-  // Move a janela de visualização para esconder a barra de URL do navegador.
-  window.scrollTo(0, 1);
-}
+  function hideURLbar() {
+    // Move a janela de visualização para esconder a barra de URL do navegador.
+    window.scrollTo(0, 1);
+  }
