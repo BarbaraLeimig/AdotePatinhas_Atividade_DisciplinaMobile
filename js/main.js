b0fe46a07290a156862ea545b7a4d7cdfa047537
BarbaraLeimig <barbaraleimigdev@gmail.com>
2024-03-21 23:13:03 -0300

    service-worker ok

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 let newWorker;
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/service-worker.js").then((reg) => {
-        reg.addEventListener("updatefound", () => {
-          newWorker = reg.installing;
-        });
-      });
-      let refreshing;
-      navigator.serviceWorker.addEventListener("controllerchange", function () {
-        if (refreshing) return;
-        window.location.reload();
-        refreshing = true;
-      });
-    }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js").then((reg) => {
+    reg.addEventListener("updatefound", () => {
+      newWorker = reg.installing;
+    });
+  });
+  let refreshing;
+  navigator.serviceWorker.addEventListener("controllerchange", function () {
+    if (refreshing) return;
+    window.location.reload();
+    refreshing = true;
+  });
+}
