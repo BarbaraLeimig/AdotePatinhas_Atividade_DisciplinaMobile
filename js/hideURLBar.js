0d56bc2d61ef95695af010dd80afbeb12bda6cbc
BarbaraLeimig <barbaraleimigdev@gmail.com>
2024-03-22 10:04:55 -0300

    service worker register ok

--- js/hideURLBar.js
+++ js/hideURLBar.js
@@ -1,13 +1,1 @@
-addEventListener(
-    "load",
-    function () {
-      // Chama a função hideURLbar assim que a página é carregada.
-      setTimeout(hideURLbar, 0);
-    },
-    false
-  );
-
-  function hideURLbar() {
-    // Move a janela de visualização para esconder a barra de URL do navegador.
-    window.scrollTo(0, 1);
-  }
+addEventListener("load",function(){setTimeout(hideURLbar,0)},!1);function hideURLbar(){window.scrollTo(0,1)}
